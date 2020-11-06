4f49ed88bcea42b61896021fdacdbc1375c90968
Hemanth21k <satyasaihemanth.p18@iiits.in>
2020-11-06 13:45:26 +0530

    added urls

--- SeniorSidekick/backend/Sunshine/api/urls.py
+++ SeniorSidekick/backend/Sunshine/api/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
-
+from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     # path('article/', article_list),
     path('services/', ServicesAPIView.as_view()),
     path('service/<int:id>/', ServicesDetailsView.as_view()),
     path('profiles/', ProfileAPIView.as_view()),
     path('profile/<int:id>/', ProfileDetailsView.as_view()),
+    path('elders/',ElderListView.as_view()),
+    path('elders/<int:id>/',ElderDetailView.as_view()),
+    path('volunteers/<int:id>/',GetVolunteers.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
