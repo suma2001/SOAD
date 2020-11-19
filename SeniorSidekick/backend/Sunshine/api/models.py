5e98ab2807b887b1b6ad69f2222ade6b4b7770fe
suma2001 <44520637+suma2001@users.noreply.github.com>
2020-11-19 12:28:15 +0530

    Auth BE done

--- SeniorSidekick/backend/Sunshine/api/models.py
+++ SeniorSidekick/backend/Sunshine/api/models.py
@@ -1,10 +1,28 @@
-# from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.gis.db import models as models
-from django.contrib.auth.models import User, AbstractBaseUser
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User, AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .managers import CustomUserManager
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    CHOICES = (
+        (1, 'Elder'),
+        (2, "Volunteer"),
+    )
+
+    # user_type = models.PositiveSmallIntegerField(choices=CHOICES, default=1)
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return str(self.username)
+
 
 class Service(models.Model):
     # service_id = models.AutoField(primary_key=True, auto_created=True)
@@ -14,6 +32,9 @@ class Service(models.Model):
     def __str__(self):
         return self.name
 
+class Experience(models.Model):
+    date_of_service = models.DateTimeField(auto_now_add=True)
+    type_of_service = models.ForeignKey('Service', on_delete=models.CASCADE)
 
 class Address(models.Model):
     # address_id = models.AutoField(primary_key=True, auto_created=True)
@@ -25,10 +46,31 @@ class Address(models.Model):
     country = models.CharField(max_length=100)
     pincode = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.city + ", " + self.state
 
-class Experience(models.Model):
-    date_of_service = models.DateTimeField(auto_now_add=True)
-    type_of_service = models.ForeignKey('Service', on_delete=models.CASCADE)
+class TestVolunteer(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="test_volunteer", null=True)
+    volunteer_age = models.IntegerField(validators=[MinValueValidator(5), MaxValueValidator(100)])
+    phone_no = models.CharField(max_length=10)
+    address = models.ForeignKey('Address', on_delete=models.PROTECT)
+    location = models.PointField(null=True)
+    availability = models.BooleanField(default=False)
+    services_available = models.ForeignKey('Service', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Elder(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="elder")
+    elder_age = models.IntegerField(validators=[MinValueValidator(20), MaxValueValidator(110)])
+    phone_no = models.CharField(max_length=10)
+    location = models.PointField(null=True)
+    address = models.ForeignKey('Address', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Feedback(models.Model):
@@ -48,57 +90,4 @@ class Feedback(models.Model):
 
     def __str__(self):
         return str(self.volunteer_name) + str(self.time)
-
-
-class Volunteer(models.Model):
-    # user=models.OneToOneField(User,on_delete=models.CASCADE)
-    # volunteer_id = models.AutoField(primary_key=True, auto_created=True)
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-    volunteer_age = models.IntegerField(validators=[MinValueValidator(5), MaxValueValidator(100)])
-    phone_no = models.CharField(max_length=10)
-    address = models.ForeignKey('Address', on_delete=models.PROTECT)
-    location = models.PointField(default=None)
-    biography = models.TextField(blank=True)
-    availability = models.BooleanField(default=False)
-    services_available = models.ForeignKey('Service', on_delete=models.CASCADE)
-    experience = ArrayField(
-        models.IntegerField()
-    )
-
-    # USERNAME_FIELD = 'email'
-    # EMAIL_FIELD = 'email'
-    # REQUIRED_FIELDS = []
-
-    def __str__(self):
-        return str(self.username)
-
-
-# @receiver(post_save,sender=User)
-# def update_volunteer_signal(sender,instance,created,**kwargs):
-#     if created:
-#         Volunteer.objects.create(user=instance)
-#     instance.profile.save()
-
-class Elder(models.Model):
-    # user=models.OneToOneField(User,on_delete=models.CASCADE)
-    # elder_id = models.AutoField(primary_key=True, auto_created=True)
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-    elder_age = models.IntegerField(validators=[MinValueValidator(20), MaxValueValidator(110)])
-    phone_no = models.CharField(max_length=10)
-    location = models.PointField(default=None)
-    address = models.ForeignKey('Address', on_delete=models.PROTECT)
-
-    def __str__(self):
-        return str(self.username)
-
-# @receiver(post_save,sender=User)
-# def update_elder_signal(sender,instance,created,**kwargs):
-#     if created:
-#         Elder.objects.create(user=instance)
-#     instance.elder.save()
+        
