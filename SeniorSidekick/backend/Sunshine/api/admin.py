8a6588a4e2ce9b256f8a805f39714cafdb854628
suma2001 <44520637+suma2001@users.noreply.github.com>
2020-11-19 12:27:36 +0530

    Auth BE done

--- SeniorSidekick/backend/Sunshine/api/admin.py
+++ SeniorSidekick/backend/Sunshine/api/admin.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
 from .models import *
 from django.contrib.gis.admin import OSMGeoAdmin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+admin.site.register(CustomUser,UserAdmin)
 admin.site.register(Service)
 admin.site.register(Address)
-admin.site.register(Experience)
-admin.site.register(Feedback)
+admin.site.register(TestVolunteer, OSMGeoAdmin)
 admin.site.register(Elder, OSMGeoAdmin)
-admin.site.register(Volunteer, OSMGeoAdmin)
-
-# @admin.register(Elder)
-# class ElderAdmin(OSMGeoAdmin):
-#     list_display = ('email', 'password', 'elder_name', 'elder_age', 'phone_no',
-#                     'address', 'location')
-
-
-# @admin.register(Volunteer)
-# class VolunteerAdmin(OSMGeoAdmin):
-#     list_display = ('email', 'password', 'volunteer_name', 'volunteer_age', 'phone_number',
-#                     'address', 'location', 'biography', 'availability', 'services_available',
-#                     'experience')
+admin.site.register(Feedback)
